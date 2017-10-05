a571508878bf6967ca0b9401b510585412801cab
afx31 <post4wjm@gmail.com>
2017-10-05 14:13:35 +1100

    ceebs

--- UserReputationSystem/UserReputationSystem/UserHandler.cs
+++ UserReputationSystem/UserReputationSystem/UserHandler.cs
@@ -9,42 +9,40 @@ namespace UserReputationSystem
 {
     class UserHandler
     {
-        //private List<User> users;
-        public List<string> users = new List<string>();
-        private User loggedInUser;
-        public User LoggedInUser;
+        //static ??
+        //private List<string> users = new List<string>();
+        //change to private
+        public List<User> users;
 
-        /*public UserHandler()     //think its stored in here?
+
+        //private User loggedInUser;
+        //public User LoggedInUser;
+
+        public UserHandler()     //think its stored in here?
         {
             users = new List<User>();
-        }*/
+        }
         public void LoadAllUsers()
         {
             string adminFilePath = @"D:\Desktop\GitHub\.NET-Assignment-2\UserReputationSystem\UserReputationSystem\bin\Debug\Admin.txt";
-            
-            using (var reader = new StreamReader(adminFilePath))
+            User _user = new User();            
+
+            StreamReader srAdmin = new StreamReader(adminFilePath);
+            while (!srAdmin.EndOfStream)
+            {
+                //LOAD EMPLOYEES                   
+                users.Add(_user);                
+            }
+            srAdmin.Close();     //passes 'true' value  
+
+            /*using (var reader = new StreamReader(adminFilePath))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    users.Add(line);
+                    //users.Add(line);
                 }
-            }
-
-            /*StreamReader srAdmin = new StreamReader(adminFilePath);
-            User _user = new User();
-            while (!srAdmin.EndOfStream)
-            {
-                //LOAD EMPLOYEES
-                //_user = new User();                    
-                users.Add(_user);
-
-                //possible functions to call
-                //_user.LoadEmployee(srAdmin);
             }*/
-
-
-            //srAdmin.Close();     //passes 'true' value            
         }
 
         /*public bool SaveAllUsers()
